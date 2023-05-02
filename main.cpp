a10cdd97fa378d903ea97292fbabe7e04c60c313
ashk123 <ashkqwe123ww@gmail.com>
2023-05-02 07:11:22 +0000

    add desktop file

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char* argv[]) {
 	auto app = Gtk::Application::create("org.gtkmm.example");
-	// Glib::RefPtr<Gtk::MediaFile> mediafile = Gtk::MediaFile::create_for_filename("a.wav");
-    // mediafile->set_loop(true);
-    // mediafile->play();
+	Glib::RefPtr<Gtk::MediaFile> mediafile = Gtk::MediaFile::create_for_filename("../media/s.mp3");
+    	mediafile->set_loop(true);
+    	mediafile->play();
 	return app->make_window_and_run<MyWindow>(argc, argv);
 }
